e6ba1cab7e39f2e2a2984b2cfa31566dd5b41e6d
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-12-25 23:37:21 +0000

    fixed check file structure...

---
+++ scripts/backup.ps1
@@ -0,0 +1,27 @@
+# Script: backup.ps1
+
+function BackupSettings {
+    try {
+        $currentSettings = Import-PowerShellDataFile -Path .\scripts\settings.psd1
+        $currentSettings | Export-Clixml -Path $currentSettings.BackupLocation
+        Write-Host "Settings backed up to $($currentSettings.BackupLocation)"
+    } catch {
+        Write-Host "Error in backup: $_" -ForegroundColor Red
+    }
+}
+
+function RestoreSettings {
+    try {
+        $backupLocation = ".\backup\settingsBackup.psd1"
+        if (Test-Path $backupLocation) {
+            $restoredSettings = Import-Clixml -Path $backupLocation
+            Write-Host "Settings restored from backup"
+        } else {
+            Write-Host "Backup file not found"
+        }
+    } catch {
+        Write-Host "Error in restore: $_" -ForegroundColor Red
+    }
+}
+
+Export-ModuleMember -Function BackupRestoreMenu, BackupSettings, RestoreSettings
