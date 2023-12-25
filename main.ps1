21975d0e64b26a0fd3839c1b14d49e3c2b0cc105
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-12-25 19:17:11 +0000

    End of session 2

--- main.ps1
+++ main.ps1
@@ -4,6 +4,19 @@
 . .\scripts\ascii.ps1
 . .\scripts\settings.psd1
 
+# Variables
+$global:choice = $null
+$global:networkChoice = $null
+$global:updateChoice = $null
+$global:cacheChoice = $null
+$global:backupChoice = $null
+$global:serviceStatus = $null
+$global:adapter = $null
+$global:currentSettings = $null
+$global:backupLocation = $null
+$global:PF = $null
+$global:cachePath = $null
+
 # Get-Admin
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host "This script requires administrator privileges. Please run as Administrator." -ForegroundColor Red
@@ -13,15 +26,15 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 # Function Show Mainmenu
 function Show-MainMenu {
     Clear-Host
-    Write-Host "NetOptX-Psc - Network Optimization and Configuration Management"
+    Write-Host "NetOptSet-Psc - Network Optimization and Configuration Management"
     Write-Host "1. Network Tweaks and Optimization"
     Write-Host "2. Windows Updates Management"
     Write-Host "3. Cache Management"
     Write-Host "4. Backup and Restore Settings"
     Write-Host "5. Exit"
-    $choice = Read-Host "Please enter your choice"
+    $global:choice = Read-Host "Please enter your choice"
     try {
-        switch ($choice) {
+        switch ($global:choice) {
             '1' { Invoke-NetworkTweaks }
             '2' { Manage-WindowsUpdates }
             '3' { Manage-Cache }
@@ -191,22 +204,108 @@ function Manage-Cache {
 }
 
 # Function Backuprestore Settings
-function BackupRestore-Settings {
-    Write-Host "Backup and Restore Settings"
-    $settings = @{ "AutoTuningLevel" = "disabled"; "DnsClientCache" = "cleared" }
-    $settings | Export-Clixml -Path ".\backup\settingsBackup.xml"
-    Write-Host "Settings backed up"
-    if (Test-Path ".\backup\settingsBackup.xml") {
-        $restoredSettings = Import-Clixml -Path ".\backup\settingsBackup.xml"
-        try {
-            Write-Host "Settings restored from backup"
-        } catch {
-            Write-Host "Error in Backup/Restore: $_" -ForegroundColor Red
-        }
-    } else {
-        Write-Host "Backup file not found"
+function BackupSettings {
+    try {
+        $currentSettings = Import-PowerShellDataFile -Path .\scripts\settings.psd1
+        $currentSettings | Export-Clixml -Path $currentSettings.BackupLocation
+        Write-Host "Settings backed up to $($currentSettings.BackupLocation)"
+    } catch {
+        Write-Host "Error in backup: $_" -ForegroundColor Red
     }
 }
+
+
+function RestoreSettings {
+    try {
+        $backupLocation = ".\backup\settingsBackup.psd1" # Update this path as needed
+        if (Test-Path $backupLocation) {
+            $restoredSettings = Import-Clixml -Path $backupLocation
+            # Apply the settings as required
+            Write-Host "Settings restored from backup"
+        } else {
+            Write-Host "Backup file not found"
+        }
+    } catch {
+        Write-Host "Error in restore: $_" -ForegroundColor Red
+    }
+}
+
+function DisableEdgeUpdates {
+    try {
+        # Determine the Program Files directory based on OS architecture
+        $PF = if ([Environment]::Is64BitOperatingSystem) { ${env:ProgramFiles(x86)} } else { $env:ProgramFiles }
+
+        # Check if Microsoft Edge is installed
+        if (Test-Path "$PF\Microsoft\Edge\Application\msedge.exe") {
+            # Stop related processes and services
+            Stop-Process -Name "MicrosoftEdgeUpdate" -Force -ErrorAction SilentlyContinue
+            Stop-Process -Name "msedge" -Force -ErrorAction SilentlyContinue
+            Stop-Service -Name "edgeupdate" -Force -ErrorAction SilentlyContinue
+            Stop-Service -Name "edgeupdatem" -Force -ErrorAction SilentlyContinue
+            Stop-Service -Name "MicrosoftEdgeElevationService" -Force -ErrorAction SilentlyContinue
+
+            # Remove related scheduled tasks
+            schtasks.exe /Delete /TN \MicrosoftEdgeUpdateBrowserReplacementTask /F
+            schtasks.exe /Delete /TN \MicrosoftEdgeUpdateTaskMachineCore /F
+            schtasks.exe /Delete /TN \MicrosoftEdgeUpdateTaskMachineUA /F
+
+            # Remove update directory
+            Remove-Item "$PF\Microsoft\EdgeUpdate" -Recurse -Force
+
+            Write-Host "Microsoft Edge updates disabled successfully."
+        } else {
+            Write-Host "Microsoft Edge is not installed."
+        }
+    } catch {
+        Write-Host "Error disabling Edge updates: $_" -ForegroundColor Red
+    }
+}
+
+
+function Clear-CacheForBrowser {
+    param (
+        [string]$BrowserName,
+        [string]$RegPath,
+        [scriptblock]$CachePathScriptBlock
+    )
+
+    if (Test-Path $RegPath) {
+        Write-Host "$BrowserName detected."
+        $cachePath = & $CachePathScriptBlock
+        if ($cachePath -and (Test-Path $cachePath)) {
+            Remove-Item $cachePath -Recurse -Force
+            Write-Host "$BrowserName cache cleared."
+        } else {
+            Write-Host "Cache path for $BrowserName not found."
+        }
+    } else {
+        Write-Host "$BrowserName not installed."
+    }
+}
+
+function ClearBrowserCaches {
+    try {
+        Clear-CacheForBrowser -BrowserName "Firefox" -RegPath "HKLM:\SOFTWARE\Mozilla\Mozilla Firefox" -CachePathScriptBlock {
+            "C:\Users\$env:USERNAME\AppData\Local\Mozilla\Firefox\Profiles\*\cache2\entries"
+        }
+
+        Clear-CacheForBrowser -BrowserName "Internet Explorer" -RegPath "HKLM:\SOFTWARE\Microsoft\Internet Explorer" -CachePathScriptBlock {
+            RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+            $null # No path to return
+        }
+
+        Clear-CacheForBrowser -BrowserName "Microsoft Edge" -RegPath "HKLM:\SOFTWARE\Microsoft\Edge" -CachePathScriptBlock {
+            "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Cache"
+        }
+
+        # Additional browsers can be added here following the same pattern
+    } catch {
+        Write-Host "Error clearing browser caches: $_" -ForegroundColor Red
+    }
+}
+
+
+# Main Loop
 while ($true) {
     Show-MainMenu
 }
